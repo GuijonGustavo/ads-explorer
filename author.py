6cdcc7484cf21530769078edcd3f956168b5f039
acf-hub <artemi89@hotmail.com>
2021-06-22 20:12:07 -0500

    pequeño arreglo de inputs

--- author.py
+++ author.py
@@ -4,16 +4,19 @@ import ads
 #ads.config.token = 'my token'
 
 class author:
-    def __init__(self, name, orcid=None):
+    def __init__(self, name=None, orcid=None):
         self.name = name
         self.orcid = orcid
+
+        if name == None and orcid == None:
+            print('WARNING: No identifier supplied, please supply either the name, the ORCID or both')
 
     #comprobar si nombre es iterable y si no si es un string
 
     def papers(self):
         name = self.name
         orcid = self.orcid
-        if not isinstance(name, str):
+        if not isinstance(name, str) and name != None:
             papers_name = list(ads.SearchQuery(author=name[0]))
             for kname in range(len(name)):
                 papers_name += list(ads.SearchQuery(author=name[kname]))
@@ -25,3 +28,6 @@ class author:
         papers_all = papers_name + papers_orcid
 
         return papers_all
+camps = author(['camps,a','camps-farina'])
+camps_papers = camps.papers()
+print(camps_papers[0].title)
